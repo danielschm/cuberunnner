96187d977085a4f72c43a21bd1a0ccbcbe17a1d8
Daniel Schmitz <daniel.schmitz@gicom.org>
2019-01-08 15:45:19 +0100

    + Added initial server code

---
+++ server/server.js
@@ -0,0 +1,27 @@
+// ------ packages -------------------------------
+const express = require("express");
+const path = require("path");
+
+const INDEX = path.join(__dirname, "/webcontent/index.html");
+const NOTFOUND = path.join(__dirname, "/webcontent/notFound.html");
+
+function fnRouting(req, res) {
+    if (req.url === "/") {
+        res.sendFile(INDEX);
+    } else {
+        res.sendFile(NOTFOUND);
+    }
+}
+
+// Start server
+const
+    app = express()
+        .use(express.static("webcontent"))
+        .use(fnRouting)
+        .listen(PORT, () => console.log("Listening on " + ip + ":" + PORT + "\n." + "\n." + "\n.\n"));
+
+const io = require("socket.io")(app);
+
+io.on("connection", function (socket) {
+    console.log("User connected: " + socket.handshake.address);
+});
