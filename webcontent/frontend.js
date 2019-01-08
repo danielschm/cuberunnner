d76f998661a2853451e42616da244cba3947b597
Martin Rempel <martin.rempel@gicom.org>
2019-01-08 16:46:37 +0100

    some frontend development

--- webcontent/frontend.js
+++ webcontent/frontend.js
@@ -1,5 +1,23 @@
 "use strict";
 
+function fnResizeHandler(CANVAS) {
+	CANVAS.width = this.innerWidth;
+	CANVAS.height = (this.innerWidth / (16 / 9));
+
+	console.log(this.innerHeight, this.innerWidth)
+}
+
 document.addEventListener("DOMContentLoaded", () => {
-	const CANVAS = document.getElementById("canvas");
+	const
+		CANVAS = new Canvas(),
+		SOCKET = io();
+
+	window.addEventListener("resize", fnResizeHandler.bind(window, CANVAS));
+
+	function todo(data) {
+		// data = { cells: [[],[]], player: {} }
+
+		let x = data.cells.length;
+		let y = data.cells[0].length;
+	}
 }, false);
--- webcontent/frontendEngine/Canvas.js
+++ webcontent/frontendEngine/Canvas.js
@@ -1,8 +1,9 @@
 class Canvas {
     constructor() {
-
+        this._CANVAS = document.getElementById("canvas");
     }
 
+    // TODO: static?
     static drawCell(x,y) {
 
     }
